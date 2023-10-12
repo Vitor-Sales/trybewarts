89850918d9b52c03051415556e3c0aa963271b56
Vitor Sales <vitormalisa@gmail.com>
2023-10-11 23:16:59 -0300

    Requisito 3 feito

---
+++ script.js
@@ -0,0 +1,15 @@
+const entrarBtn = document.getElementById('button');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+
+const checkEmail = () => {
+  const emailText = 'tryber@teste.com';
+  const passwordText = '123456';
+  if (email.value === emailText && password.value === passwordText) {
+    window.alert('Olá, Tryber!');
+  } else {
+    window.alert('Email ou senha inválidos');
+  }
+};
+
+entrarBtn.addEventListener('click', checkEmail);
