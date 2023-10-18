c564c99c4c83acc757760fbd3cae324ab693f692
Vitor Sales <vitormalisa@gmail.com>
2023-10-18 12:57:57 -0300

    Requisito 18 feito

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const entrarBtn = document.getElementById('button');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
+const agreement = document.getElementById('agreement');
+const submitBtn = document.getElementById('submit-btn');
 
 const checkEmail = () => {
   const emailText = 'tryber@teste.com';
@@ -12,4 +14,11 @@ const checkEmail = () => {
   }
 };
 
-entrarBtn.addEventListener('click', checkEmail);
+const ableSubmit = () => {
+  submitBtn.disabled = !agreement.checked;
+};
+
+window.onload = () => {
+  entrarBtn.addEventListener('click', checkEmail);
+  agreement.addEventListener('change', ableSubmit);
+};
